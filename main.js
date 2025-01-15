125abe5782a8e0ac9e3704bd783252d45361fb8f
alharharasami <alharharasami@gmail.com>
2025-01-15 22:20:22 +0300

    first solution

---
+++ main.js
@@ -0,0 +1,32 @@
+/*== Understanding the sort() method ==*/
+
+/*=== alpha sort order ===*/
+const alphaArray = ["zebra", "arial", "medium"];
+const alphaArray2 = ["zebra", "arial", "medium", 3, 1, 1000];
+
+const alphaSort = (array) => {
+    if (!array.length) return;
+    return array.sort();
+}
+
+console.log(alphaSort(alphaArray));
+console.log(alphaSort(alphaArray2));
+
+/*=== Solution 1: Sort numbers accurately ===*/
+
+const arrayOfNumbers = [1, 100000, 21, 30, 4];
+
+const numberSortAsc = (array) => {
+    if (!array.length) return;
+    return array.sort((a, b) => a - b);
+}
+
+console.log(numberSortAsc(arrayOfNumbers));
+
+/*=== Solution 2: Sort numbers in descending order ===*/
+const numberSortDesc = (array) => {
+    if (!array.length) return;
+    return array.sort((a, b) => a - b).reverse();
+}
+
+console.log(numberSortDesc(arrayOfNumbers));
